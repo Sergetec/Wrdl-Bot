d902b02737de5cd668028f22fadbea49e83b9e46
Sergetec <sergetec2000@gmail.com>
2023-01-03 19:31:55 +0200

    inactivity message fix

--- Events/ready.js
+++ Events/ready.js
@@ -25,55 +25,58 @@ module.exports = {
         client.user.setStatus('online')
 
         //Check for inactive games
-        let Guilds = client.guilds.cache.map(guild => guild.id)
-        for (let guild of Guilds){
-            guild = client.guilds.cache.get(guild)
-            const check = async () => {
-                const query = {
-                    guildID: guild.id,
-                    expires: { $lt: new Date() },
-                }
-                const results = await gamesSchema.find(query)
+        try {
+            let Guilds = client.guilds.cache.map(guild => guild.id)
+            for (let guild of Guilds) {
+                guild = client.guilds.cache.get(guild)
+                const check = async () => {
+                    const query = {
+                        guildID: guild.id,
+                        expires: {$lt: new Date()},
+                    }
+                    const results = await gamesSchema.find(query)
 
-                const query2 = {
-                    guildID: guild.id,
-                }
+                    const query2 = {
+                        guildID: guild.id,
+                    }
 
-                if (!isIterable(results)){
-                    let channel = results.channelStarted
+                    if (!isIterable(results)) {
+                        let channel = results.channelStarted
 
-                    const message = new MessageEmbed()
-                        .setTitle('Wordle Game')
-                        .setColor('RED')
-                        .setDescription(`Game for <@${results.userID}> has ended due to inactivity`)
-                    await client.channels.cache.get(channel).send({ embeds: [message] })
-                    await gamesSchema.deleteMany(query)
-
-                    let schema = await statsSchema.findOne(query2)
-                    schema.gamesLost = schema.gamesLost + 1
-                    schema.winRate = Math.trunc(schema.gamesWon / schema.gamesTotal * 100)
-                    await schema.save()
-                }
-                else{
-                    for (const result of results){
-                        let channel = result.channelStarted
-
-                        const message2 = new MessageEmbed()
+                        const message = new MessageEmbed()
                             .setTitle('Wordle Game')
                             .setColor('RED')
-                            .setDescription(`Game for <@${result.userID}> has ended due to inactivity`)
-                        await client.channels.cache.get(channel).send({ embeds: [message2] })
+                            .setDescription(`<@${results.userID}>'s game has ended due to inactivity`)
+                        await client.channels.cache.get(channel).send({embeds: [message]})
                         await gamesSchema.deleteMany(query)
 
-                        let schema2 = await statsSchema.findOne(query2)
-                        schema2.gamesLost = schema2.gamesLost + 1
-                        schema2.winRate = Math.trunc(schema2.gamesWon / schema2.gamesTotal * 100)
-                        await schema2.save()
+                        let schema = await statsSchema.findOne(query2)
+                        schema.gamesLost = schema.gamesLost + 1
+                        schema.winRate = Math.trunc(schema.gamesWon / schema.gamesTotal * 100)
+                        await schema.save()
+                    } else {
+                        for (const result of results) {
+                            let channel = result.channelStarted
+
+                            const message2 = new MessageEmbed()
+                                .setTitle('Wordle Game')
+                                .setColor('RED')
+                                .setDescription(`<@${result.userID}>' game has ended due to inactivity`)
+                            await client.channels.cache.get(channel).send({embeds: [message2]})
+                            await gamesSchema.deleteMany(query)
+
+                            let schema2 = await statsSchema.findOne(query2)
+                            schema2.gamesLost = schema2.gamesLost + 1
+                            schema2.winRate = Math.trunc(schema2.gamesWon / schema2.gamesTotal * 100)
+                            await schema2.save()
+                        }
                     }
+                    setTimeout(check, 1000 * 30)
                 }
-                setTimeout(check, 1000 * 30)
+                await check()
             }
-            await check()
+        } catch (err) {
+            console.log(err)
         }
     }
 }
