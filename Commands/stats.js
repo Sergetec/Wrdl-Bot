5298e9cb0730970b12a596e60548d66ffdec84d3
Sergetec <96439803+Sergetec@users.noreply.github.com>
2023-10-12 08:53:44 +0300

    test for fonts

--- Commands/stats.js
+++ Commands/stats.js
@@ -11,8 +11,8 @@ const GREEN = '#5c8d4d'
 const GRAY = '#3a3a3c'
 const LIGHT_GRAY = '#818384'
 const WHITE = '#ffffff'
-GlobalFonts.registerFromPath('/Wrdl-Bot/Fonts/ARLRDBD.ttf', { family: "Arial Rounded MT Bold"})
-GlobalFonts.registerFromPath('/Wrdl-Bot/Fonts/micross.ttf', 'Microsoft Sans Serif')
+GlobalFonts.registerFromPath('./ARLRDBD.ttf', "Arial Rounded MT Bold")
+GlobalFonts.registerFromPath('./micross.ttf', 'Microsoft Sans Serif')
 const FONT_FAMILY_ARIAL_ROUNDED = 'Arial Rounded MT Bold'
 const FONT_FAMILY_SANS_SERIF = 'Microsoft Sans Serif'
 
