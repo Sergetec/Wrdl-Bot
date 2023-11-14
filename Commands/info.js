d7c3ad9a7826dc8e10c9575c06dcac8e6479604a
Sergetec <sergetec2000@gmail.com>
2023-11-14 15:33:07 +0200

    Text fix

--- Commands/info.js
+++ Commands/info.js
@@ -8,7 +8,7 @@ module.exports = {
     async execute(client, interaction) {
         const embed = new EmbedBuilder()
             .setTitle('❓ HOW TO PLAY')
-            .setDescription('Guess the **word** in 6 tries.\n\nEach guess must be a **valid 5-letter word**. Use `/guess` to make your guess, then press enter to submit.\n\nAfter each guess, the color of the letters will change to show how close your guess was to the word.\n\n__Examples:__\n• The letter **R** and **O** are in the word but in the wrong place.\n• The letter **A** is in the right place in the word.\n• All the **gray** letters are nowhere in the word.')
+            .setDescription('Guess the **word** in 6 tries.\n\nEach guess must be a **valid 5-letter word**. Use `/guess` to make your guess, then press enter to submit.\n\nAfter each guess, the color of the letters will change to show how close your guess was to the word.\n\n__Examples:__\n• The letters **R** and **O** are in the word but in the wrong place.\n• The letter **A** is in the right place in the word.\n• All the **gray** letters are nowhere in the word.')
             .setColor(GREEN)
             .setImage('https://cdn.discordapp.com/attachments/1071147069982642246/1163805144262127719/image.png?ex=6540e900&is=652e7400&hm=e9aab45b85ea689def57bc319fc8cb3a1306d540777c7b27df3b3ac8beac9e37&')
         return await interaction.reply({ embeds: [embed] })
