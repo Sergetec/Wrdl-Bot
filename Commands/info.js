966a9fae110834e4f13e035f4745f1cb794d4290
Sergetec <sergetec2000@gmail.com>
2023-12-15 17:44:09 +0200

    Updated image

--- Commands/info.js
+++ Commands/info.js
@@ -10,7 +10,7 @@ module.exports = {
             .setTitle('❓ HOW TO PLAY')
             .setDescription('Guess the **word** in 6 tries.\n\nEach guess must be a **valid 5-letter word**. Use `/guess` to make your guess, then press enter to submit.\n\nAfter each guess, the color of the letters will change to show how close your guess was to the word.\n\n__Examples:__\n• The letters **R** and **O** are in the word but in the wrong place.\n• The letter **A** is in the right place in the word.\n• All the **gray** letters are nowhere in the word.')
             .setColor(GREEN)
-            .setImage('https://cdn.discordapp.com/attachments/1071147069982642246/1163805144262127719/image.png?ex=6540e900&is=652e7400&hm=e9aab45b85ea689def57bc319fc8cb3a1306d540777c7b27df3b3ac8beac9e37&')
+            .setImage('https://cdn.discordapp.com/attachments/1071147069982642246/1185225567767888035/image.png?ex=658ed653&is=657c6153&hm=6fa5ecbe82e09ac45028bd5bb5fce4e6630856e66fea1f6d02159d6dae09f82f&')
         return await interaction.reply({ embeds: [embed] })
     }
 }
