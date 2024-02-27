3b497f8da001e4cc75c32edb70db673fc1ec4c2e
Sergetec <sergetec2000@gmail.com>
2024-02-27 23:11:09 +0200

    Fix for streak leaderboard

--- Commands/leaderboard.js
+++ Commands/leaderboard.js
@@ -144,21 +144,18 @@ async function getTopWins(client, interaction, results, canvas, context, actionR
         if (i === 0) {
             context.fillText(`${fetchUser.username}`, canvas.width / 2, canvas.height / 2 - 480)
             let image = await loadImage('./Images/1st_place_medal.png')
-            // await drawMedal_drawWins(context, image, results, canvas.height / 2 - 400, i)
             context.drawImage(image, canvas.width / 2 - 100, canvas.height / 2 - 450, 200, 200)
             continue
         }
         if (i === 1) {
             context.fillText(`${fetchUser.username}`, canvas.width / 2 - 500, canvas.height / 2 - 280)
             let image = await loadImage('./Images/2nd_place_medal.png')
-            // await drawMedal_drawWins(context, image, results, canvas.height / 2 - 250, i)
             context.drawImage(image, canvas.width / 2 - 600, canvas.height / 2 - 250, 200, 200)
             continue
         }
         if (i === 2) {
             context.fillText(`${fetchUser.username}`, canvas.width / 2 + 500, canvas.height / 2 - 280)
             let image = await loadImage('./Images/3rd_place_medal.png')
-            // await drawMedal_drawWins(context, image, results, canvas.height / 2 - 250, i)
             context.drawImage(image, canvas.width / 2 + 400, canvas.height / 2 - 250, 200, 200)
             continue
         }
@@ -168,7 +165,6 @@ async function getTopWins(client, interaction, results, canvas, context, actionR
         }
         context.font = `bold 40px ${FONT_FAMILY_EXO}`
         context.fillText(`${fetchUser.username}`, x, y)
-        // await drawMedal_drawWins(context, null, results, y, i)
         x += 600
         if (count % 3 == 0) { // decrease height
             y += 180
@@ -187,44 +183,50 @@ async function getTopWins(client, interaction, results, canvas, context, actionR
 }
 
 async function getTopStreak(client, interaction, results, canvas, context, actionRow) {
-    let background = await loadImage('./Images/background2.png')
-    context.drawImage(background, 0, 0, 600, 500)
+    let background = await loadImage('./Images/background3.png')
+    context.drawImage(background, 0, 0, 1920, 1097)
+    let fire = await loadImage('./Images/fire_color.png')
+    context.drawImage(fire, 34, 29, 110, 110)
     let totalPlayers = await statsSchema.count()
-    let count = 3, y = 30
+    let count = 3, x = 1920 / 2 - 600, y = 1097 / 2 + 90
     for (let i = 0; i < results.length; ++i) {
         context.fillStyle = WHITE
-        context.textAlign = "left"
-        context.font = `bold 18px ${FONT_FAMILY_EXO}`
+        context.textAlign = "center"
+        context.font = `bold 60px ${FONT_FAMILY_EXO}`
         if (i === 10) {
             break
         }
         let id = results[i].userID
         const fetchUser = await client.users.fetch(id)
         if (i === 0) {
-            context.fillText(`${fetchUser.username}`, 100, y)
-            let image = await loadImage('./Images/1st_place_medal_color.png')
-            await drawMedal_drawStreak(context, image, results, y, i)
-            y += 50
+            context.fillText(`${fetchUser.username}`, canvas.width / 2, canvas.height / 2 - 480)
+            let image = await loadImage('./Images/1st_place_medal.png')
+            context.drawImage(image, canvas.width / 2 - 100, canvas.height / 2 - 450, 200, 200)
             continue
         }
         if (i === 1) {
-            context.fillText(`${fetchUser.username}`, 100, y)
-            let image = await loadImage('./Images/2nd_place_medal_color.png')
-            await drawMedal_drawStreak(context, image, results, y, i)
-            y += 50
+            context.fillText(`${fetchUser.username}`, canvas.width / 2 - 500, canvas.height / 2 - 280)
+            let image = await loadImage('./Images/2nd_place_medal.png')
+            context.drawImage(image, canvas.width / 2 - 600, canvas.height / 2 - 250, 200, 200)
             continue
         }
         if (i === 2) {
-            context.fillText(`${fetchUser.username}`, 100, y)
-            let image = await loadImage('./Images/3rd_place_medal_color.png')
-            await drawMedal_drawStreak(context, image, results, y, i)
-            y += 50
+            context.fillText(`${fetchUser.username}`, canvas.width / 2 + 500, canvas.height / 2 - 280)
+            let image = await loadImage('./Images/3rd_place_medal.png')
+            context.drawImage(image, canvas.width / 2 + 400, canvas.height / 2 - 250, 200, 200)
             continue
         }
         count++
-        context.fillText(`${count}. ${fetchUser.username}`, 10, y)
-        await drawMedal_drawStreak(context, null, results, y, i)
-        y += 50
+        if (count === 10) {
+            x += 600
+        }
+        context.font = `bold 40px ${FONT_FAMILY_EXO}`
+        context.fillText(`${fetchUser.username}`, x, y)
+        x += 600
+        if (count % 3 == 0) { // decrease height
+            y += 180
+            x = 1920 / 2 - 600
+        }
     }
     const file = new AttachmentBuilder(await canvas.toBuffer(), { name: 'leaderboard.png' })
     const embed = new EmbedBuilder()
@@ -235,26 +237,4 @@ async function getTopStreak(client, interaction, results, canvas, context, actio
             text: `Top ${results.length} out of ${totalPlayers} players`
         })
     return await interaction.editReply({ content: '', embeds: [embed], files: [file], components: [actionRow] })
-}
-
-async function drawMedal_drawWins(context, image, results, y, i) {
-    context.textAlign = "right"
-    context.font = `18px ${FONT_FAMILY_EXO}`
-    if (image !== null) {
-        context.drawImage(image, 75, y - 20, 187, 187)
-        context.fillText(`${results[i].gamesWon} wins`, canvas.width - 60, y)
-    } else {
-        context.fillText(`${results[i].gamesWon} wins`, canvas.width - 10, y)
-    }
-}
-
-async function drawMedal_drawStreak(context, image, results, y, i) {
-    context.textAlign = "right"
-    context.font = `18px ${FONT_FAMILY_EXO}`
-    if (image !== null) {
-        context.drawImage(image, 75, y - 20, 25, 25)
-        context.fillText(`${results[i].maxStreak} streak`, canvas.width - 60, y)
-    } else {
-        context.fillText(`${results[i].maxStreak} streak`, canvas.width - 10, y)
-    }
 }
